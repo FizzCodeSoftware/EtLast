50471c808532eb13f58d6c7c77b2cb4e7b908bec
gerleim <gerleim@gmail.com>
2019-04-25 14:04:36 +0200

    Refactor TestInitialize.

--- Tests/EtLast.Tests.EPPlus/ReadExcelSampleTests.cs
+++ Tests/EtLast.Tests.EPPlus/ReadExcelSampleTests.cs
@@ -9,8 +9,11 @@
     [TestClass]
     public class ReadExcelSampleTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IOperationProcess _process;
+        private EpPlusExcelReaderProcess _epPlusExcelReaderProcess;
+
+        [TestInitialize]
+        public void Initialize()
         {
             var operationProcessConfiguration = new OperationProcessConfiguration()
             {
@@ -20,7 +23,22 @@
 
             var context = new EtlContext<DictionaryRow>();
 
-            var epPlusProcess = new OperationProcess(context, "EpPlusProcess")
+            _epPlusExcelReaderProcess = new EpPlusExcelReaderProcess(context, "EpPlusExcelReaderProcess")
+            {
+                FileName = @"..\..\TestData\Sample.xlsx",
+                ColumnMap = new List<(string ExcelColumn, string RowColumn, ITypeConverter Converter, object ValueIfNull)>
+                    {
+                        ("Id", "Id", new StringConverter(), string.Empty),
+                        ("Name", "Name", new StringConverter(), string.Empty),
+                        ("Value1", "ValueString", new StringConverter(), string.Empty),
+                        ("Value2", "ValueInt", new IntConverter(), null),
+                        ("Value3", "ValueDate", new DateConverter(), null),
+                        ("Value4", "ValueDouble", new DoubleConverter(), null)
+                    },
+                SheetName = "Sheet1"
+            };
+
+            _process = new OperationProcess(context, "EpPlusProcess")
             {
                 Configuration = operationProcessConfiguration,
                 InputProcess = new EpPlusExcelReaderProcess(context, "EpPlusExcelReaderProcess")
@@ -39,9 +57,13 @@
                 }
             };
 
-            epPlusProcess.AddOperation(new ThrowExceptionOnRowErrorOperation());
+            _process.AddOperation(new ThrowExceptionOnRowErrorOperation());
+        }
 
-            List<IRow> result = epPlusProcess.Evaluate().ToList();
+        [TestMethod]
+        public void ReadExcelSample()
+        {
+            List<IRow> result = _process.Evaluate().ToList();
             Assert.AreEqual(2, result.Count);
         }
     }
