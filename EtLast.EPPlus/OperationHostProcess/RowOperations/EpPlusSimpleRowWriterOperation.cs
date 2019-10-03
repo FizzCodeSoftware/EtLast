78df3e95fb49f598c37e390bb94896c08c5d47e4
Lengyel János Zoltán <lengyel.janos@gmail.com>
2019-10-03 09:03:44 +0200

    add EpPlusSimpleRowWriterOperation

---
+++ EtLast.EPPlus/OperationHostProcess/RowOperations/EpPlusSimpleRowWriterOperation.cs
@@ -0,0 +1,81 @@
+﻿namespace FizzCode.EtLast.EPPlus
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using OfficeOpenXml;
+
+    public class EpPlusSimpleRowWriterOperation : AbstractRowOperation
+    {
+        public string FileName { get; set; }
+        public ExcelPackage Source { get; set; }
+        public string SheetName { get; set; }
+        public List<ColumnCopyConfiguration> ColumnConfiguration { get; set; }
+
+        private ExcelPackage _excelPackage;
+        private SimpleExcelWriterState _state;
+
+        public override void Apply(IRow row)
+        {
+            if (_excelPackage == null) // lazy load here instead of prepare
+            {
+                _excelPackage = Source ?? new ExcelPackage(new FileInfo(FileName));
+
+                _state.LastWorksheet = _excelPackage.Workbook.Worksheets.Add(SheetName);
+                _state.LastRow = 1;
+                _state.LastCol = 1;
+                foreach (var col in ColumnConfiguration)
+                {
+                    _state.LastWorksheet.Cells[_state.LastRow, _state.LastCol].Value = col.ToColumn;
+                    _state.LastCol++;
+                }
+
+                _state.LastRow++;
+            }
+
+            try
+            {
+                _state.LastCol = 1;
+                foreach (var col in ColumnConfiguration)
+                {
+                    _state.LastWorksheet.Cells[_state.LastRow, _state.LastCol].Value = row[col.FromColumn];
+                    _state.LastCol++;
+                }
+
+                _state.LastRow++;
+            }
+            catch (Exception ex)
+            {
+                var exception = new OperationExecutionException(Process, this, row, "error raised during writing an excel file", ex);
+                exception.AddOpsMessage(string.Format(CultureInfo.InvariantCulture, "error raised during writing an excel file, file name: {0}, message: {1}, row: {2}", FileName, ex.Message, row.ToDebugString()));
+                exception.Data.Add("FileName", FileName);
+                exception.Data.Add("SheetName", SheetName);
+                throw exception;
+            }
+        }
+
+        public override void Prepare()
+        {
+            if (string.IsNullOrEmpty(FileName))
+                throw new OperationParameterNullException(this, nameof(FileName));
+            if (string.IsNullOrEmpty(SheetName))
+                throw new OperationParameterNullException(this, nameof(SheetName));
+            if (ColumnConfiguration == null)
+                throw new OperationParameterNullException(this, nameof(ColumnConfiguration));
+
+            _state = new SimpleExcelWriterState();
+        }
+
+        public override void Shutdown()
+        {
+            base.Shutdown();
+
+            _state = null;
+
+            _excelPackage.Save();
+            _excelPackage.Dispose();
+            _excelPackage = null;
+        }
+    }
+}
--- EtLast.EPPlus/OperationHostProcess/RowOperations/SimpleExcelWriterState.cs
+++ EtLast.EPPlus/OperationHostProcess/RowOperations/SimpleExcelWriterState.cs
@@ -5,7 +5,7 @@
     public class SimpleExcelWriterState
     {
         public ExcelWorksheet LastWorksheet { get; set; }
-        public int LastRow { get; set; }
-        public int LastCol { get; set; }
+        public int LastRow { get; set; } = 1;
+        public int LastCol { get; set; } = 1;
     }
 }
