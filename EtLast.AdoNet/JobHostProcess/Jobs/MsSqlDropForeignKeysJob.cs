c49d3934502a5210567c5c1362979eb394256a0a
Lengyel János Zoltán <lengyel.janos@gmail.com>
2019-10-14 15:07:19 +0200

    add more MsSqlDropForeignKeysJobMode variants

--- EtLast.AdoNet/JobHostProcess/Jobs/MsSqlDropForeignKeysJob.cs
+++ EtLast.AdoNet/JobHostProcess/Jobs/MsSqlDropForeignKeysJob.cs
@@ -9,14 +9,14 @@
     using System.Transactions;
     using FizzCode.DbTools.Configuration;
 
-    public enum MsSqlDropForeignKeysJobMode { All, SpecifiedTables, SpecifiedSchema }
+    public enum MsSqlDropForeignKeysJobMode { All, InSpecifiedTables, InSpecifiedSchema, ToSpecifiedSchema, ToSpecifiedTables }
 
     public class MsSqlDropForeignKeysJob : AbstractSqlStatementsJob
     {
         /// <summary>
-        /// Default value is <see cref="MsSqlDropForeignKeysJobMode.SpecifiedTables"/>
+        /// Default value is <see cref="MsSqlDropForeignKeysJobMode.ToSpecifiedTables"/>
         /// </summary>
-        public MsSqlDropForeignKeysJobMode Mode { get; set; } = MsSqlDropForeignKeysJobMode.SpecifiedTables;
+        public MsSqlDropForeignKeysJobMode Mode { get; set; } = MsSqlDropForeignKeysJobMode.ToSpecifiedTables;
 
         /// <summary>
         /// Table names must include schema name.
@@ -33,11 +33,11 @@
 
             switch (Mode)
             {
-                case MsSqlDropForeignKeysJobMode.SpecifiedTables:
+                case MsSqlDropForeignKeysJobMode.InSpecifiedTables:
                     if (TableNames == null || TableNames.Length == 0)
                         throw new JobParameterNullException(process, this, nameof(TableNames));
                     if (!string.IsNullOrEmpty(SchemaName))
-                        throw new InvalidJobParameterException(process, this, nameof(SchemaName), SchemaName, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.SpecifiedTables));
+                        throw new InvalidJobParameterException(process, this, nameof(SchemaName), SchemaName, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.InSpecifiedTables));
                     break;
                 case MsSqlDropForeignKeysJobMode.All:
                     if (TableNames != null)
@@ -45,9 +45,15 @@
                     if (!string.IsNullOrEmpty(SchemaName))
                         throw new InvalidJobParameterException(process, this, nameof(SchemaName), SchemaName, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.All));
                     break;
-                case MsSqlDropForeignKeysJobMode.SpecifiedSchema:
+                case MsSqlDropForeignKeysJobMode.InSpecifiedSchema:
                     if (TableNames != null)
-                        throw new InvalidJobParameterException(process, this, nameof(TableNames), TableNames, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.All));
+                        throw new InvalidJobParameterException(process, this, nameof(TableNames), TableNames, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.InSpecifiedSchema));
+                    if (string.IsNullOrEmpty(SchemaName))
+                        throw new JobParameterNullException(process, this, nameof(SchemaName));
+                    break;
+                case MsSqlDropForeignKeysJobMode.ToSpecifiedSchema:
+                    if (TableNames != null)
+                        throw new InvalidJobParameterException(process, this, nameof(TableNames), TableNames, "Value must be null if " + nameof(Mode) + " is set to " + nameof(MsSqlDropForeignKeysJobMode.ToSpecifiedSchema));
                     if (string.IsNullOrEmpty(SchemaName))
                         throw new JobParameterNullException(process, this, nameof(SchemaName));
                     break;
@@ -66,47 +72,110 @@
                 try
                 {
                     command.CommandTimeout = CommandTimeout;
-                    command.CommandText = "select * from INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_TYPE='FOREIGN KEY'";
-                    if (Mode == MsSqlDropForeignKeysJobMode.SpecifiedSchema)
+                    switch (Mode)
                     {
-                        command.CommandText += " and TABLE_SCHEMA = @schemaName";
-                        var parameter = command.CreateParameter();
-                        parameter.ParameterName = "schemaName";
-                        parameter.Value = SchemaName;
-                        command.Parameters.Add(parameter);
+                        case MsSqlDropForeignKeysJobMode.ToSpecifiedSchema:
+                            {
+                                command.CommandText = @"
+select
+	fk.[name] fkName,
+	SCHEMA_NAME(fk.schema_id) schemaName,
+	OBJECT_NAME(fk.parent_object_id) tableName
+from
+	sys.foreign_keys fk
+	inner join sys.foreign_key_columns fkc on fk.object_id = fkc.constraint_object_id
+	inner join sys.objects o on fkc.referenced_object_id = o.object_id and o.schema_id = SCHEMA_ID(@schemaName)";
+                                var parameter = command.CreateParameter();
+                                parameter.ParameterName = "schemaName";
+                                parameter.Value = SchemaName;
+                                command.Parameters.Add(parameter);
+                                break;
+                            }
+
+                        case MsSqlDropForeignKeysJobMode.InSpecifiedSchema:
+                            {
+                                command.CommandText = @"
+select
+	fk.[name] fkName,
+	SCHEMA_NAME(fk.schema_id) schemaName,
+	OBJECT_NAME(fk.parent_object_id) tableName
+from
+	sys.foreign_keys fk
+	inner join sys.foreign_key_columns fkc on fk.object_id = fkc.constraint_object_id
+where fk.schema_id = SCHEMA_ID(@schemaName)";
+                                var parameter = command.CreateParameter();
+                                parameter.ParameterName = "schemaName";
+                                parameter.Value = SchemaName;
+                                command.Parameters.Add(parameter);
+                                break;
+                            }
+
+                        case MsSqlDropForeignKeysJobMode.InSpecifiedTables:
+                            command.CommandText = @"
+select
+	fk.[name] fkName,
+	SCHEMA_NAME(fk.schema_id) schemaName,
+	OBJECT_NAME(fk.parent_object_id) tableName
+from
+	sys.foreign_keys fk
+	inner join sys.foreign_key_columns fkc on fk.object_id = fkc.constraint_object_id";
+                            break;
+                        case MsSqlDropForeignKeysJobMode.ToSpecifiedTables:
+                            command.CommandText = @"
+select
+	fk.[name] fkName,
+	SCHEMA_NAME(fk.schema_id) schemaName,
+	OBJECT_NAME(fk.parent_object_id) tableName, 
+	SCHEMA_NAME(o.schema_id) refSchemaName,
+	o.name refTableName
+from
+	sys.foreign_keys fk
+	inner join sys.foreign_key_columns fkc on fk.object_id = fkc.constraint_object_id
+	inner join sys.objects o on fkc.referenced_object_id = o.object_id";
+                            break;
                     }
+
+                    // tables are not filtered with an IN clause due to the limitations of the query processor
+                    // this solution will read unnecessary data, but it will work in all conditions
 
                     process.Context.Log(LogSeverity.Debug, process, "({Job}) querying foreign key names from {ConnectionStringKey} with SQL statement {SqlStatement}, timeout: {Timeout} sec, transaction: {Transaction}",
                         Name, ConnectionString.Name, command.CommandText, command.CommandTimeout, Transaction.Current?.TransactionInformation.CreationTime.ToString("yyyy.MM.dd HH:mm:ss.ffff", CultureInfo.InvariantCulture) ?? "NULL");
 
-                    var tablesNamesHashSet = Mode == MsSqlDropForeignKeysJobMode.SpecifiedTables
+                    var tablesNamesHashSet = Mode == MsSqlDropForeignKeysJobMode.InSpecifiedTables || Mode == MsSqlDropForeignKeysJobMode.ToSpecifiedTables
                         ? TableNames.Select(x => x.ToLowerInvariant()).ToHashSet()
                         : null;
 
-                    var tableConstraintNames = new Dictionary<string, List<string>>();
+                    var constraintsByTable = new Dictionary<string, List<string>>();
 
                     using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            var tableName = "[" + (string)reader["TABLE_SCHEMA"] + "].[" + (string)reader["TABLE_NAME"] + "]";
+                            var sourceTableName = "[" + (string)reader["schemaName"] + "].[" + (string)reader["tableName"] + "]";
 
-                            if (Mode == MsSqlDropForeignKeysJobMode.SpecifiedTables && !tablesNamesHashSet.Contains(tableName))
+                            if (Mode == MsSqlDropForeignKeysJobMode.InSpecifiedTables && !tablesNamesHashSet.Contains(sourceTableName))
                                 continue;
 
-                            if (!tableConstraintNames.TryGetValue(tableName, out var list))
+                            if (Mode == MsSqlDropForeignKeysJobMode.ToSpecifiedTables)
                             {
-                                list = new List<string>();
-                                tableConstraintNames.Add(tableName, list);
+                                var referredTableName = "[" + (string)reader["refSchemaName"] + "].[" + (string)reader["refTableName"] + "]";
+                                if (!tablesNamesHashSet.Contains(sourceTableName))
+                                    continue;
                             }
 
-                            list.Add("[" + (string)reader["CONSTRAINT_NAME"] + "]");
+                            if (!constraintsByTable.TryGetValue(sourceTableName, out var list))
+                            {
+                                list = new List<string>();
+                                constraintsByTable.Add(sourceTableName, list);
+                            }
+
+                            list.Add("[" + (string)reader["fkName"] + "]");
                         }
                     }
 
                     _tableNames = new List<string>();
                     var statements = new List<string>();
-                    foreach (var kvp in tableConstraintNames.OrderBy(x => x.Key))
+                    foreach (var kvp in constraintsByTable.OrderBy(x => x.Key))
                     {
                         _tableNames.Add(kvp.Key);
 
@@ -116,11 +185,11 @@
                     var modeInfo = Mode switch
                     {
                         MsSqlDropForeignKeysJobMode.All => " (all foreign keys in database)",
-                        MsSqlDropForeignKeysJobMode.SpecifiedSchema => " (in schema '" + SchemaName + "')",
+                        MsSqlDropForeignKeysJobMode.InSpecifiedSchema => " (in schema '" + SchemaName + "')",
                         _ => null,
                     };
 
-                    process.Context.Log(LogSeverity.Information, process, "{ForeignKeyCount} foreign keys aquired from information schema in {Elapsed} for {TableCount} tables" + modeInfo, tableConstraintNames.Sum(x => x.Value.Count), startedOn.Elapsed, _tableNames.Count);
+                    process.Context.Log(LogSeverity.Information, process, "{ForeignKeyCount} foreign keys aquired from information schema in {Elapsed} for {TableCount} tables" + modeInfo, constraintsByTable.Sum(x => x.Value.Count), startedOn.Elapsed, _tableNames.Count);
 
                     return statements;
                 }
