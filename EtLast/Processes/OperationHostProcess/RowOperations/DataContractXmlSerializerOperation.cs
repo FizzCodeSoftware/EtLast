0bbdd9ebcb6a4a48e69eab10bdf83af2ca086ac6
Lengyel János Zoltán <lengyel.janos@gmail.com>
2019-12-18 14:20:53 +0100

    add counters to DataContractXmlSerializerOperation

--- EtLast/Processes/OperationHostProcess/RowOperations/DataContractXmlSerializerOperation.cs
+++ EtLast/Processes/OperationHostProcess/RowOperations/DataContractXmlSerializerOperation.cs
@@ -1,6 +1,7 @@
 ﻿namespace FizzCode.EtLast
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
     using System.Runtime.Serialization;
     using System.Xml;
@@ -23,6 +24,8 @@
             if (sourceObject == null)
                 return;
 
+            var startedOn = Stopwatch.StartNew();
+
             try
             {
                 using (var ms = new MemoryStream())
@@ -35,17 +38,32 @@
 
                     var data = ms.ToArray();
                     row.SetValue(ColumnConfiguration.ToColumn, data, this);
+
+                    var time = startedOn.Elapsed;
+
+                    CounterCollection.IncrementTimeSpan("serialization time (DataContract XML)", time);
+                    CounterCollection.IncrementCounter("serialization count (DataContract XML)", 1);
+
+                    CounterCollection.IncrementDebugTimeSpan("serialization time (DataContract XML) - " + TypeHelpers.GetFriendlyTypeName(typeof(T)), time);
+                    CounterCollection.IncrementDebugCounter("serialization count (DataContract XML) - " + TypeHelpers.GetFriendlyTypeName(typeof(T)), 1);
                 }
             }
             catch (Exception ex)
             {
+                var time = startedOn.Elapsed;
+                CounterCollection.IncrementTimeSpan("serialization time (DataContract XML) - error", time);
+                CounterCollection.IncrementCounter("serialization count (DataContract XML) - error", 1);
+
+                CounterCollection.IncrementDebugTimeSpan("serialization time (DataContract XML) - error - " + TypeHelpers.GetFriendlyTypeName(typeof(T)), time);
+                CounterCollection.IncrementDebugCounter("serialization count (DataContract XML) - error - " + TypeHelpers.GetFriendlyTypeName(typeof(T)), 1);
+
                 switch (ActionIfFailed)
                 {
                     case InvalidValueAction.SetSpecialValue:
                         row.SetValue(ColumnConfiguration.ToColumn, SpecialValueIfFailed, this);
                         break;
                     case InvalidValueAction.Throw:
-                        throw new OperationExecutionException(Process, this, row, "DataContract serialization failed", ex);
+                        throw new OperationExecutionException(Process, this, row, "DataContract XML serialization failed", ex);
                     case InvalidValueAction.RemoveRow:
                         Process.RemoveRow(row, this);
                         return;
@@ -55,7 +73,7 @@
                             Process = Process,
                             Operation = this,
                             OriginalValue = null,
-                            Message = "DataContract serialization failed: " + ex.Message,
+                            Message = "DataContract XML serialization failed: " + ex.Message,
                         }, this);
                         break;
                 }
