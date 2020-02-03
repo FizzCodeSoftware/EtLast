bd090b30518fdbf1961c5bb21cae677b9c11be78
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-02-03 10:22:49 +0100

    HungarianGiroBankAccountConverter

---
+++ EtLast.Specialized/HungarianGiroBankAccountConverter.cs
@@ -0,0 +1,102 @@
+﻿namespace FizzCode.EtLast.Specialized
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    public class HungarianGiroBankAccountConverter : StringConverter
+    {
+        /// <summary>
+        /// Default true.
+        /// </summary>
+        public bool AutomaticallyAddHyphens { get; set; } = true;
+
+        private static readonly int[] _checkSumNumbers = new[] { 9, 7, 3, 1, 9, 7, 3 };
+
+        public HungarianGiroBankAccountConverter(string formatHint = null, IFormatProvider formatProviderHint = null)
+            : base(formatHint, formatProviderHint)
+        {
+            RemoveSpacesFromMiddle = true;
+            RemoveLineBreaksFromMiddle = true;
+            TrimStartEnd = true;
+        }
+
+        public override object Convert(object source)
+        {
+            var taxNr = base.Convert(source) as string;
+
+            return Convert(taxNr, AutomaticallyAddHyphens);
+        }
+
+        public static string Convert(string accountNr, bool automaticallyAddHyphens)
+        {
+            if (string.IsNullOrEmpty(accountNr))
+                return null;
+
+            if (!Validate(accountNr))
+                return null;
+
+            if (automaticallyAddHyphens && !accountNr.Contains("-", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return accountNr.Length switch
+                {
+                    16 => accountNr.Substring(0, 8) + "-" + accountNr.Substring(8, 8),
+                    24 => accountNr.Substring(0, 8) + "-" + accountNr.Substring(8, 8) + "-" + accountNr.Substring(16, 8),
+                    _ => accountNr,
+                };
+            }
+
+            return accountNr;
+        }
+
+        public static bool Validate(string accountNr)
+        {
+            string[] parts;
+
+            if (!accountNr.Contains("-", StringComparison.InvariantCultureIgnoreCase))
+            {
+                switch (accountNr.Length)
+                {
+                    case 16:
+                        parts = new[] { accountNr.Substring(0, 8), accountNr.Substring(8, 8) };
+                        break;
+                    case 24:
+                        parts = new[] { accountNr.Substring(0, 8), accountNr.Substring(8, 8), accountNr.Substring(16, 8) };
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            else
+            {
+                parts = accountNr.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                if ((parts.Length != 2 && parts.Length != 3) || parts.Any(x => x.Length != 8))
+                    return false;
+            }
+
+            foreach (var part in parts)
+            {
+                var digitSum = 0;
+                for (var i = 0; i < 7; i++)
+                {
+                    if (!int.TryParse(part[i].ToString(CultureInfo.InvariantCulture), out var digit))
+                        return false;
+
+                    digitSum += digit * _checkSumNumbers[i];
+                }
+
+                var checkSum = digitSum % 10;
+                if (checkSum != 0)
+                    checkSum = 10 - checkSum;
+
+                if (!int.TryParse(part[7].ToString(CultureInfo.InvariantCulture), out var lastDigit))
+                    return false;
+
+                if (lastDigit != checkSum)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
