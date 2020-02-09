d8ea94d6d25d4d788c96cecdacfdadcf4c681892
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-02-09 10:34:29 +0100

    added simple merge finalizer

---
+++ EtLast.DwhBuilder.Alpha/TableBuilderExtensions/SimpleMergeFinalizer/AddSimpleMergeFinalizer.cs
@@ -0,0 +1,89 @@
+﻿namespace FizzCode.EtLast.DwhBuilder.Alpha
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FizzCode.DbTools.DataDefinition;
+    using FizzCode.EtLast.AdoNet;
+
+    public static partial class TableBuilderExtensions
+    {
+        public static DwhTableBuilder[] AddSimpleMergeFinalizer(this DwhTableBuilder[] builders, Action<SimpleMergeFinalizerBuilder> customizer)
+        {
+            foreach (var tableBuilder in builders)
+            {
+                var tempBuilder = new SimpleMergeFinalizerBuilder(tableBuilder);
+                customizer.Invoke(tempBuilder);
+
+                if (tempBuilder.KeyColumns == null)
+                    throw new NotSupportedException("you must specify the key columns of " + nameof(RemoveExistingRows) + " for table " + tableBuilder.Table.TableName);
+
+                tableBuilder.AddFinalizerCreator(_ => CreateAddSimpleMergeFinalizer(tempBuilder));
+            }
+
+            return builders;
+        }
+
+        private static IEnumerable<IExecutable> CreateAddSimpleMergeFinalizer(SimpleMergeFinalizerBuilder builder)
+        {
+            var pk = builder.TableBuilder.SqlTable.Properties.OfType<PrimaryKey>().FirstOrDefault();
+            var pkIsIdentity = pk.SqlColumns.Any(c => c.SqlColumn.HasProperty<Identity>());
+            var currentEtlRunId = builder.TableBuilder.DwhBuilder.Context.AdditionalData.GetAs("CurrentEtlRunId", 0);
+
+            var mergeIntoBaseColumns = builder.TableBuilder.SqlTable.Columns
+                .Where(x => !x.HasProperty<IsEtlRunInfoColumnProperty>());
+
+            if (pkIsIdentity)
+            {
+                mergeIntoBaseColumns = mergeIntoBaseColumns
+                    .Where(x => pk.SqlColumns.All(pkc => !string.Equals(pkc.SqlColumn.Name, x.Name, StringComparison.InvariantCultureIgnoreCase)));
+            }
+
+            var mergeIntoBaseColumnNames = mergeIntoBaseColumns
+                .Select(c => builder.TableBuilder.DwhBuilder.ConnectionString.Escape(c.Name))
+                .ToArray();
+
+            var columnNamesToMatch = builder.KeyColumns
+                .Select(c => builder.TableBuilder.DwhBuilder.ConnectionString.Escape(c))
+                .ToArray();
+
+            var parameters1 = new Dictionary<string, object>();
+            if (builder.TableBuilder.EtlInsertRunIdColumnNameEscaped != null || builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped != null)
+                parameters1.Add("EtlRunId", currentEtlRunId);
+
+            var columnNamesToUpdate = builder.TableBuilder.SqlTable.Columns
+                .Where(x => !x.HasProperty<IsEtlRunInfoColumnProperty>()
+                    && pk.SqlColumns.All(pkc => !string.Equals(pkc.SqlColumn.Name, x.Name, StringComparison.InvariantCultureIgnoreCase))
+                    && builder.KeyColumns.All(kc => !string.Equals(x.Name, kc, StringComparison.InvariantCultureIgnoreCase)))
+                .Select(c => builder.TableBuilder.DwhBuilder.ConnectionString.Escape(c.Name))
+                .ToArray();
+
+            yield return new CustomMsSqlMergeSqlStatementProcess(builder.TableBuilder.DwhBuilder.Context, "MergeIntoBase", builder.TableBuilder.Table.Topic)
+            {
+                ConnectionString = builder.TableBuilder.Table.Scope.Configuration.ConnectionString,
+                CommandTimeout = 60 * 60,
+                SourceTableName = builder.TableBuilder.Table.TempTableName,
+                TargetTableName = builder.TableBuilder.Table.TableName,
+                SourceTableAlias = "s",
+                TargetTableAlias = "t",
+                OnCondition = string.Join(" and ", columnNamesToMatch.Select(x => "t." + x + "=s." + x)),
+                WhenMatchedAction = columnNamesToUpdate.Length > 0 || (builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped != null)
+                    ? "UPDATE SET "
+                        + string.Join(", ", columnNamesToUpdate.Select(c => "t." + c + "=s." + c))
+                        + (builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped != null
+                            ? (columnNamesToUpdate.Length > 0 ? ", " : "") + builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped + "=@EtlRunId"
+                            : "")
+                    : null,
+                WhenNotMatchedByTargetAction = "INSERT (" + string.Join(", ", mergeIntoBaseColumnNames)
+                    + (builder.TableBuilder.EtlInsertRunIdColumnNameEscaped != null ? ", " + builder.TableBuilder.EtlInsertRunIdColumnNameEscaped : "")
+                    + (builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped != null ? ", " + builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped : "")
+                    + ") VALUES ("
+                        + string.Join(", ", mergeIntoBaseColumnNames.Select(c => "s." + c))
+                        + (builder.TableBuilder.EtlInsertRunIdColumnNameEscaped != null ? ", @EtlRunId" : "")
+                        + (builder.TableBuilder.EtlUpdateRunIdColumnNameEscaped != null ? ", @EtlRunId" : "")
+                    + ")",
+                Parameters = parameters1,
+            };
+        }
+    }
+}
---
+++ EtLast.DwhBuilder.Alpha/TableBuilderExtensions/SimpleMergeFinalizer/SimpleMergeFinalizerBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace FizzCode.EtLast.DwhBuilder.Alpha
+{
+    using System;
+    using System.Linq;
+    using FizzCode.DbTools.DataDefinition;
+
+    public class SimpleMergeFinalizerBuilder
+    {
+        internal DwhTableBuilder TableBuilder { get; }
+        internal string[] KeyColumns { get; private set; }
+
+        internal SimpleMergeFinalizerBuilder(DwhTableBuilder tableBuilder)
+        {
+            TableBuilder = tableBuilder;
+        }
+
+        public SimpleMergeFinalizerBuilder UsePrimaryKey()
+        {
+            var pk = TableBuilder.SqlTable.Properties.OfType<PrimaryKey>().FirstOrDefault();
+            if (pk == null)
+                throw new NotSupportedException();
+
+            KeyColumns = pk.SqlColumns.Select(x => x.SqlColumn.Name).ToArray();
+            return this;
+        }
+
+        public SimpleMergeFinalizerBuilder UseSpecificKeyColumns(params string[] keyColumns)
+        {
+            KeyColumns = keyColumns;
+            return this;
+        }
+    }
+}
