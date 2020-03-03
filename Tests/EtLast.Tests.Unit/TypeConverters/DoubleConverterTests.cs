c47e0b2d15810fd96ab4bedfcceae63f024c1359
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-03-03 14:49:30 +0100

    simplify double converter tests

--- Tests/EtLast.Tests.Unit/TypeConverters/DoubleConverterTests.cs
+++ Tests/EtLast.Tests.Unit/TypeConverters/DoubleConverterTests.cs
@@ -1,108 +1,34 @@
 ﻿namespace FizzCode.EtLast.Tests.Unit.TypeConverters
 {
+    using System.Globalization;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class DoubleConverterTests
     {
         [TestMethod]
-        public void InvString()
+        [DataRow("1", 1d)]
+        [DataRow("1234.456", 1234.456d)]
+        [DataRow("12345678901234567890", 1.2345678901234567E+19)]
+        [DataRow((sbyte)77, 77d)]
+        [DataRow((byte)77, 77d)]
+        [DataRow((short)77, 77d)]
+        [DataRow((ushort)77, 77d)]
+        [DataRow(77, 77d)]
+        [DataRow((uint)77, 77d)]
+        [DataRow((long)77, 77d)]
+        [DataRow((ulong)77, 77d)]
+        [DataRow(3f, 3d)]
+        [DataRow(3.12d, 3.12d)]
+        public void DoubleConverter(object input, double? expected)
         {
             var converter = new DoubleConverter();
-            var result = converter.Convert("1.234");
-            Assert.AreEqual(1.234d, result);
+            var result = converter.Convert(input);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
-        public void InvStringThousands()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert("1234.456");
-            Assert.AreEqual(1234.456d, result);
-        }
-
-        [TestMethod]
-        public void FromSByte()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert((sbyte)77);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromByte()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert((byte)77);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromShort()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert((short)77);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromUShort()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert((ushort)77);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromInt()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(77);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromUInt()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(77u);
-            Assert.AreEqual(77d, result);
-        }
-
-        [TestMethod]
-        public void FromLong()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(long.MaxValue);
-            Assert.AreEqual((double)long.MaxValue, result);
-        }
-
-        [TestMethod]
-        public void FromULong()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(ulong.MaxValue);
-            Assert.AreEqual((double)ulong.MaxValue, result);
-        }
-
-        [TestMethod]
-        public void FromFloat()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(4f / 5f);
-            Assert.AreEqual((double)(4f / 5f), result);
-        }
-
-        [TestMethod]
-        public void FromDouble()
-        {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(4d / 5d);
-            Assert.AreEqual(4d / 5d, result);
-        }
-
-        [TestMethod]
-        public void FromDecimal()
+        public void DoubleConverterFromDecimal()
         {
             var converter = new DoubleConverter();
             var result = converter.Convert(4m / 5m);
@@ -110,11 +36,29 @@
         }
 
         [TestMethod]
-        public void FromDecimalBig()
+        [DataRow("1.234", 1.234d)]
+        [DataRow("-1.234", -1.234d)]
+        [DataRow("1,234", 1.234d, "hu-HU")]
+        [DataRow("+1,234", 1.234d, "hu-HU")]
+        [DataRow(" +1,234  ", 1.234d, "hu-HU")]
+        [DataRow("+ 1,234", null, "hu-HU")]
+        [DataRow("1   234 456,2 ", 1234456.2d, "hu-HU")]
+        [DataRow("-1 234 456,2", -1234456.2d, "hu-HU")]
+        [DataRow("- 1 234 456,2", null, "hu-HU")]
+        [DataRow("1.234", 1.234d, "hu-HU")]
+        [DataRow("1,234,456.2", 1234456.2, "hu-HU")]
+        [DataRow("123,2", 1232d, DisplayName = "MisrecognizedThousandSeparator")]
+        [DataRow("123,2", 123.2d, "hu-HU")]
+        [DataRow("1 234 456,2", null)]
+        [DataRow("123.2", 123.2d, "en-US")]
+        [DataRow("1,234,456.2", 1234456.2d, "en-US")]
+        [DataRow("1234456.2", 1234456.2d, "en-US")]
+        [DataRow("1.234.456,2", null, "en-US")]
+        public void DoubleConverterAuto(string input, double? expected, string locale = null)
         {
-            var converter = new DoubleConverter();
-            var result = converter.Convert(decimal.MaxValue);
-            Assert.AreEqual((double)decimal.MaxValue, result);
+            var converter = new DoubleConverterAuto(locale == null ? CultureInfo.InvariantCulture : new CultureInfo(locale));
+            var result = converter.Convert(input);
+            Assert.AreEqual(expected, result);
         }
     }
 }
