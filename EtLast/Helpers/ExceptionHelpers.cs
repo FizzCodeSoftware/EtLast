ffb88aeda69753da554a458b2871a95019cb8f8a
Lengyel János Zoltán <lengyel.janos@outlook.com>
2020-05-26 15:41:04 +0200

    fix ExceptionHelpers.FormatExceptionWithDetails when source exception has no stack trace

--- EtLast/Helpers/ExceptionHelpers.cs
+++ EtLast/Helpers/ExceptionHelpers.cs
@@ -7,95 +7,104 @@
     {
         public static string FormatExceptionWithDetails(this Exception exception, bool includeTrace = true)
         {
-            var lvl = 0;
-            var msg = "EXCEPTION: ";
-
-            var cex = exception;
-            while (cex != null)
+            try
             {
-                if (lvl > 0)
-                    msg += "\nINNER EXCEPTION: ";
+                var lvl = 0;
+                var msg = "EXCEPTION: ";
 
-                msg += cex.GetType().GetFriendlyTypeName() + ": " + cex.Message;
-
-                if (cex.Data?["Process"] is string storedProcess)
+                var cex = exception;
+                while (cex != null)
                 {
-                    msg += "\n\tPROCESS: " + storedProcess;
-                }
+                    if (lvl > 0)
+                        msg += "\nINNER EXCEPTION: ";
 
-                if (includeTrace)
-                {
-                    var trace = cex.Data?["Trace"] is string TraceCaller
-                        ? TraceCaller
-                        : EtlException.GetTraceFromStackFrames(new StackTrace(cex, true).GetFrames());
+                    msg += cex.GetType().GetFriendlyTypeName() + ": " + cex.Message;
 
-                    msg += "\n\tTRACE:\n\t\t" + trace.Replace("\n", "\n\t\t", StringComparison.Ordinal);
-                }
-
-                if (cex.Data?.Count > 0)
-                {
-                    var first = true;
-                    foreach (var key in cex.Data.Keys)
+                    if (cex.Data?["Process"] is string storedProcess)
                     {
-                        var k = key.ToString();
-                        if (k == "Process")
-                            continue;
-
-                        if (k == "CallChain")
-                            continue;
-
-                        if (k == "OpsMessage")
-                            continue;
-
-                        if (k == "Trace")
-                            continue;
-
-                        if (k == "Row")
-                            continue;
-
-                        if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("uid", StringComparison.InvariantCultureIgnoreCase))
-                            continue;
-
-                        if (first)
-                        {
-                            msg += "\n\tDATA: ";
-                            first = false;
-                        }
-                        else
-                        {
-                            msg += ", ";
-                        }
-
-                        var value = cex.Data[key];
-                        msg += "[" + k + "] = " + (value != null ? value.ToString().Trim() : "NULL");
+                        msg += "\n\tPROCESS: " + storedProcess;
                     }
-                }
 
-                if (cex.Data?["Row"] is string storedRow)
-                {
-                    msg += "\n\tROW: " + storedRow;
-                }
-
-                if (cex.Data?.Count > 0)
-                {
-                    foreach (var key in cex.Data.Keys)
+                    if (includeTrace)
                     {
-                        var k = key.ToString();
-                        if (k == "Row")
-                            continue;
+                        if (!(cex.Data?["Trace"] is string trace))
+                            trace = EtlException.GetTraceFromStackFrames(new StackTrace(cex, true).GetFrames());
 
-                        if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("uid", StringComparison.InvariantCultureIgnoreCase))
+                        if (trace != null)
                         {
-                            msg += "\n\t" + k.ToUpperInvariant() + ": " + rowStr;
+                            msg += "\n\tTRACE:\n\t\t" + trace.Replace("\n", "\n\t\t", StringComparison.Ordinal);
                         }
                     }
+
+                    if (cex.Data?.Count > 0)
+                    {
+                        var first = true;
+                        foreach (var key in cex.Data.Keys)
+                        {
+                            var k = key.ToString();
+                            if (k == "Process")
+                                continue;
+
+                            if (k == "CallChain")
+                                continue;
+
+                            if (k == "OpsMessage")
+                                continue;
+
+                            if (k == "Trace")
+                                continue;
+
+                            if (k == "Row")
+                                continue;
+
+                            if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("uid", StringComparison.InvariantCultureIgnoreCase))
+                                continue;
+
+                            if (first)
+                            {
+                                msg += "\n\tDATA: ";
+                                first = false;
+                            }
+                            else
+                            {
+                                msg += ", ";
+                            }
+
+                            var value = cex.Data[key];
+                            msg += "[" + k + "] = " + (value != null ? value.ToString().Trim() : "NULL");
+                        }
+                    }
+
+                    if (cex.Data?["Row"] is string storedRow)
+                    {
+                        msg += "\n\tROW: " + storedRow;
+                    }
+
+                    if (cex.Data?.Count > 0)
+                    {
+                        foreach (var key in cex.Data.Keys)
+                        {
+                            var k = key.ToString();
+                            if (k == "Row")
+                                continue;
+
+                            if (k.Contains("Row", StringComparison.InvariantCultureIgnoreCase) && cex.Data[key] is string rowStr && rowStr.StartsWith("uid", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                msg += "\n\t" + k.ToUpperInvariant() + ": " + rowStr;
+                            }
+                        }
+                    }
+
+                    cex = cex.InnerException;
+                    lvl++;
                 }
 
-                cex = cex.InnerException;
-                lvl++;
+                return msg;
             }
-
-            return msg;
+            catch (Exception)
+            {
+                return exception.ToString();
+            }
         }
     }
 }
