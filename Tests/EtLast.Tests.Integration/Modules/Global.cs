3f9d34f760db5d2a277a762aeb350efc9da4c27b
Lengyel János Zoltán <janos.lengyel@fizzcode.hu>
2022-07-08 15:51:08 +0200

    move global.cs

--- Tests/EtLast.Tests.Integration/Modules/Global.cs
+++ Tests/EtLast.Tests.Integration/Modules/Global.cs
@@ -1,7 +1,6 @@
 ﻿global using System;
 global using System.Collections.Generic;
 global using System.Data.Common;
-global using System.Diagnostics;
 global using System.Linq;
 global using FizzCode.DbTools;
 global using FizzCode.DbTools.Common;
