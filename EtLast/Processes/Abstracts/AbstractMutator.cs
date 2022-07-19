2aec335ec63c0f64706ac67afef3a53ed6c55eec
Lengyel János Zoltán <janos.lengyel@fizzcode.hu>
2022-07-19 17:28:53 +0200

    reduce overhead in AbstractMutator

--- EtLast/Processes/Abstracts/AbstractMutator.cs
+++ EtLast/Processes/Abstracts/AbstractMutator.cs
@@ -1,8 +1,9 @@
 ﻿namespace FizzCode.EtLast;
 
 [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)]
-public abstract class AbstractMutator : AbstractSequence, IMutator
+public abstract class AbstractMutator : AbstractProcess, IMutator
 {
+    public Action<ISequence> Initializer { get; init; }
     public ISequence Input { get; set; }
     public RowTestDelegate RowFilter { get; set; }
     public RowTagTestDelegate RowTagFilter { get; set; }
@@ -12,158 +13,240 @@ public abstract class AbstractMutator : AbstractSequence, IMutator
     {
     }
 
-    protected sealed override IEnumerable<IRow> EvaluateImpl(Stopwatch netTimeStopwatch)
+    private IEnumerable<IRow> Evaluate(IProcess caller)
     {
+        Context.RegisterProcessInvocationStart(this, caller);
+
+        if (caller != null)
+            Context.Log(LogSeverity.Information, this, "{ProcessKind} started by {Process}", Kind, caller.Name);
+        else
+            Context.Log(LogSeverity.Information, this, "{ProcessKind} started", Kind);
+
+        LogPublicSettableProperties(LogSeverity.Verbose);
+
+        var netTimeStopwatch = Stopwatch.StartNew();
         try
         {
-            StartMutator();
+            if (Input == null)
+                throw new ProcessParameterNullException(this, nameof(Input));
+
+            ValidateMutator();
         }
         catch (Exception ex)
         {
+            netTimeStopwatch.Stop();
             AddException(ex);
             yield break;
         }
 
-        var mutatedRows = new List<IRow>();
-
-        netTimeStopwatch.Stop();
-        var enumerator = Input.TakeRowsAndTransferOwnership(this).GetEnumerator();
-        netTimeStopwatch.Start();
-
-        var mutatedRowCount = 0;
-        var ignoredRowCount = 0;
-
-        while (!Context.CancellationToken.IsCancellationRequested)
+        if (!Context.CancellationToken.IsCancellationRequested)
         {
-            netTimeStopwatch.Stop();
-            var finished = !enumerator.MoveNext();
-            if (finished)
-                break;
-
-            var row = enumerator.Current;
-            netTimeStopwatch.Start();
-
-            if (row.Tag is HeartBeatTag tag)
+            if (Initializer != null)
             {
-                ProcessHeartBeatRow(row, tag);
+                try
+                {
+                    Initializer.Invoke(this);
+                }
+                catch (Exception ex)
+                {
+                    AddException(new InitializerDelegateException(this, ex));
+                    yield break;
+                }
+            }
+
+            if (!Context.CancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    StartMutator();
+                }
+                catch (Exception ex)
+                {
+                    AddException(ex);
+                    yield break;
+                }
+
+                var mutatedRows = new List<IRow>();
 
                 netTimeStopwatch.Stop();
-                yield return row;
+                var enumerator = Input.TakeRowsAndTransferOwnership(this).GetEnumerator();
                 netTimeStopwatch.Start();
-                continue;
-            }
 
-            var apply = false;
-            if (RowFilter != null)
-            {
-                try
-                {
-                    apply = RowFilter.Invoke(row);
-                }
-                catch (Exception ex)
-                {
-                    AddException(ex, row);
-                    break;
-                }
+                var mutatedRowCount = 0;
+                var ignoredRowCount = 0;
 
-                if (!apply)
+                while (!Context.CancellationToken.IsCancellationRequested)
                 {
-                    ignoredRowCount++;
                     netTimeStopwatch.Stop();
-                    yield return row;
+                    var finished = !enumerator.MoveNext();
+                    if (finished)
+                        break;
+
+                    var row = enumerator.Current;
                     netTimeStopwatch.Start();
-                    continue;
-                }
-            }
 
-            if (RowTagFilter != null)
-            {
-                try
-                {
-                    apply = RowTagFilter.Invoke(row.Tag);
-                }
-                catch (Exception ex)
-                {
-                    AddException(ex, row);
-                    break;
-                }
-
-                if (!apply)
-                {
-                    ignoredRowCount++;
-                    netTimeStopwatch.Stop();
-                    yield return row;
-                    netTimeStopwatch.Start();
-                    continue;
-                }
-            }
-
-            mutatedRowCount++;
-
-            var kept = false;
-            try
-            {
-                foreach (var mutatedRow in MutateRow(row))
-                {
-                    if (mutatedRow == row)
-                        kept = true;
-
-                    if (mutatedRow.CurrentProcess != this)
+                    if (row.Tag is HeartBeatTag tag)
                     {
-                        AddException(new ProcessExecutionException(this, mutatedRow, "mutator returned a row without proper ownership"));
+                        ProcessHeartBeatRow(row, tag);
+
+                        netTimeStopwatch.Stop();
+                        yield return row;
+                        netTimeStopwatch.Start();
+                        continue;
+                    }
+
+                    var apply = false;
+                    if (RowFilter != null)
+                    {
+                        try
+                        {
+                            apply = RowFilter.Invoke(row);
+                        }
+                        catch (Exception ex)
+                        {
+                            AddException(ex, row);
+                            break;
+                        }
+
+                        if (!apply)
+                        {
+                            ignoredRowCount++;
+                            netTimeStopwatch.Stop();
+                            yield return row;
+                            netTimeStopwatch.Start();
+                            continue;
+                        }
+                    }
+
+                    if (RowTagFilter != null)
+                    {
+                        try
+                        {
+                            apply = RowTagFilter.Invoke(row.Tag);
+                        }
+                        catch (Exception ex)
+                        {
+                            AddException(ex, row);
+                            break;
+                        }
+
+                        if (!apply)
+                        {
+                            ignoredRowCount++;
+                            netTimeStopwatch.Stop();
+                            yield return row;
+                            netTimeStopwatch.Start();
+                            continue;
+                        }
+                    }
+
+                    mutatedRowCount++;
+
+                    var kept = false;
+                    try
+                    {
+                        foreach (var mutatedRow in MutateRow(row))
+                        {
+                            if (mutatedRow == row)
+                                kept = true;
+
+                            if (mutatedRow.CurrentProcess != this)
+                            {
+                                AddException(new ProcessExecutionException(this, mutatedRow, "mutator returned a row without proper ownership"));
+                                break;
+                            }
+
+                            mutatedRows.Add(mutatedRow);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        AddException(ex, row);
                         break;
                     }
 
-                    mutatedRows.Add(mutatedRow);
+                    if (!kept)
+                    {
+                        Context.SetRowOwner(row, null);
+                    }
+
+                    netTimeStopwatch.Stop();
+                    foreach (var mutatedRow in mutatedRows)
+                    {
+                        yield return mutatedRow;
+                    }
+
+                    netTimeStopwatch.Start();
+
+                    mutatedRows.Clear();
+                }
+
+                netTimeStopwatch.Start();
+
+                try
+                {
+                    CloseMutator();
+                }
+                catch (Exception ex)
+                {
+                    AddException(ex);
+                    yield break;
+                }
+
+                if (mutatedRowCount + ignoredRowCount > 0)
+                {
+                    Context.Log(LogSeverity.Debug, this, "mutated {MutatedRowCount} of {TotalRowCount} rows in {Elapsed}/{ElapsedWallClock}",
+                        mutatedRowCount, mutatedRowCount + ignoredRowCount, InvocationInfo.LastInvocationStarted.Elapsed, netTimeStopwatch.Elapsed);
                 }
             }
-            catch (Exception ex)
-            {
-                AddException(ex, row);
-                break;
-            }
-
-            if (!kept)
-            {
-                Context.SetRowOwner(row, null);
-            }
-
-            netTimeStopwatch.Stop();
-            foreach (var mutatedRow in mutatedRows)
-            {
-                yield return mutatedRow;
-            }
-
-            netTimeStopwatch.Start();
-
-            mutatedRows.Clear();
         }
 
-        netTimeStopwatch.Start();
+        netTimeStopwatch.Stop();
+        Context.RegisterProcessInvocationEnd(this, netTimeStopwatch.ElapsedMilliseconds);
 
-        try
-        {
-            CloseMutator();
-        }
-        catch (Exception ex)
-        {
-            AddException(ex);
-            yield break;
-        }
+        Context.Log(LogSeverity.Information, this, "{ProcessKind} {ProcessResult} in {Elapsed}/{ElapsedWallClock}",
+            Kind, "finished", InvocationInfo.LastInvocationStarted.Elapsed, netTimeStopwatch.Elapsed);
+    }
 
-        if (mutatedRowCount + ignoredRowCount > 0)
+    public void Execute(IProcess caller)
+    {
+        CountRowsAndReleaseOwnership(caller);
+    }
+
+    public IEnumerable<IRow> TakeRowsAndTransferOwnership(IProcess caller)
+    {
+        foreach (var row in Evaluate(caller))
         {
-            Context.Log(LogSeverity.Debug, this, "mutated {MutatedRowCount} of {TotalRowCount} rows in {Elapsed}/{ElapsedWallClock}",
-                mutatedRowCount, mutatedRowCount + ignoredRowCount, InvocationInfo.LastInvocationStarted.Elapsed, netTimeStopwatch.Elapsed);
+            row.Context.SetRowOwner(row, caller);
+            yield return row;
         }
     }
 
-    protected sealed override void ValidateImpl()
+    public IEnumerable<ISlimRow> TakeRowsAndReleaseOwnership(IProcess caller)
     {
-        if (Input == null)
-            throw new ProcessParameterNullException(this, nameof(Input));
+        foreach (var row in Evaluate(caller))
+        {
+            if (caller != null)
+                row.Context.SetRowOwner(row, caller);
 
-        ValidateMutator();
+            row.Context.SetRowOwner(row, null);
+
+            yield return row;
+        }
+    }
+
+    public int CountRowsAndReleaseOwnership(IProcess caller)
+    {
+        var count = 0;
+        foreach (var row in Evaluate(caller))
+        {
+            row.Context.SetRowOwner(row, caller);
+            row.Context.SetRowOwner(row, null);
+
+            count++;
+        }
+
+        return count;
     }
 
     protected virtual void ValidateMutator()
---
+++ Tests/EtLast.Tests.Integration.DwhBuilderTests/Modules/DwhBuilderTests/Tasks/NetTime.cs
@@ -0,0 +1,53 @@
+﻿namespace FizzCode.EtLast.Tests.Integration.Modules.DwhBuilderTests;
+
+public class NetTime : AbstractEtlTask
+{
+    public override void ValidateParameters()
+    {
+    }
+
+    public override IEnumerable<IJob> CreateJobs()
+    {
+        yield return CreateSequence(1);
+        yield return CreateSequence(100);
+        yield return CreateSequence(10000);
+    }
+
+    private ISequence CreateSequence(int mod)
+    {
+        return SequenceBuilder.Fluent
+        .ImportEnumerable(new EnumerableImporter(Context)
+        {
+            Name = "SlowSource",
+            InputGenerator = process => DelayedInputGenerator(process, 1000000 / mod, mod),
+        })
+        .CustomCode("QuickMutator", row => true)
+        .CustomCode("SlowMutator", row =>
+        {
+            ExpensiveStuff(mod);
+            return true;
+        })
+        .Build();
+    }
+
+    private IEnumerable<IReadOnlySlimRow> DelayedInputGenerator(EnumerableImporter process, int count, int mod)
+    {
+        for (var i = 1; i <= count; i++)
+        {
+            ExpensiveStuff(mod);
+
+            yield return new SlimRow()
+            {
+                ["index"] = i,
+            };
+        }
+    }
+
+    private void ExpensiveStuff(int mod)
+    {
+        var sum = 0;
+        var n = 10000 * mod;
+        for (var i = 0; i < n; i++)
+            sum = (int)(((long)sum + i) % int.MaxValue);
+    }
+}
