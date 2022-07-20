45a7b49eedb51344be3a19a16413fce028df773d
gerleim <gerleim@gmail.com>
2022-07-20 17:45:57 +0200

    Move Test files according to EtLast.AdoNet renames.

---
+++ Tests/EtLast.Tests.Integration.AdoNetTests/Modules/AdoNetTests/Jobs/GetTableMaxValue.cs
@@ -0,0 +1,43 @@
+﻿namespace FizzCode.EtLast.Tests.Integration.Modules.AdoNetTests;
+
+public class GetTableMaxValue : AbstractEtlTask
+{
+    public NamedConnectionString ConnectionString { get; init; }
+    public string DatabaseName { get; init; }
+
+    public override void ValidateParameters()
+    {
+        if (ConnectionString == null)
+            throw new ProcessParameterNullException(this, nameof(ConnectionString));
+
+        if (DatabaseName == null)
+            throw new ProcessParameterNullException(this, nameof(DatabaseName));
+    }
+
+    public override IEnumerable<IJob> CreateJobs()
+    {
+        yield return new CustomSqlStatement(Context)
+        {
+            ConnectionString = ConnectionString,
+            SqlStatement = $"CREATE TABLE {nameof(GetTableMaxValue)} (Id INT NOT NULL, DateTimeValue DATETIME2);" +
+                    $"INSERT INTO {nameof(GetTableMaxValue)} (Id, DateTimeValue) VALUES (1, '2022.07.08');" +
+                    $"INSERT INTO {nameof(GetTableMaxValue)} (Id, DateTimeValue) VALUES (1, '2022.07.09');",
+        };
+
+        yield return new CustomJob(Context)
+        {
+            Name = nameof(GetTableMaxValue),
+            Action = job =>
+            {
+                var result = new GetTableMaxValue<DateTime>(Context)
+                {
+                    ConnectionString = ConnectionString,
+                    TableName = ConnectionString.Escape(nameof(GetTableMaxValue)),
+                    ColumnName = "DateTimeValue",
+                }.ExecuteWithResult();
+
+                Assert.AreEqual(new DateTime(2022, 7, 9), result.MaxValue);
+            }
+        };
+    }
+}
---
+++ Tests/EtLast.Tests.Integration.AdoNetTests/Modules/AdoNetTests/Jobs/GetTableRecordCount.cs
@@ -0,0 +1,42 @@
+﻿namespace FizzCode.EtLast.Tests.Integration.Modules.AdoNetTests;
+
+public class GetTableRecordCount : AbstractEtlTask
+{
+    public NamedConnectionString ConnectionString { get; init; }
+    public string DatabaseName { get; init; }
+
+    public override void ValidateParameters()
+    {
+        if (ConnectionString == null)
+            throw new ProcessParameterNullException(this, nameof(ConnectionString));
+
+        if (DatabaseName == null)
+            throw new ProcessParameterNullException(this, nameof(DatabaseName));
+    }
+
+    public override IEnumerable<IJob> CreateJobs()
+    {
+        yield return new CustomSqlStatement(Context)
+        {
+            ConnectionString = ConnectionString,
+            SqlStatement = $"CREATE TABLE {nameof(GetTableRecordCount)} (Id INT NOT NULL, DateTimeValue DATETIME2);" +
+                    $"INSERT INTO {nameof(GetTableRecordCount)} (Id, DateTimeValue) VALUES (1, '2022.07.08');" +
+                    $"INSERT INTO {nameof(GetTableRecordCount)} (Id, DateTimeValue) VALUES (1, '2022.07.09');",
+        };
+
+        yield return new CustomJob(Context)
+        {
+            Name = nameof(GetTableRecordCount),
+            Action = job =>
+            {
+                var result = new EtLast.GetTableRecordCount(Context)
+                {
+                    ConnectionString = ConnectionString,
+                    TableName = ConnectionString.Escape(nameof(GetTableRecordCount)),
+                }.ExecuteWithResult();
+
+                Assert.AreEqual(2, result);
+            }
+        };
+    }
+}
