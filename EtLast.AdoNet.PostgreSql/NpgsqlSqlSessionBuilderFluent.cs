d43c04dc37e8317df20bf4c98c525854bdf5355f
Lengyel János Zoltán <lengyel.janos@gmail.com>
2023-12-20 19:49:16 +0100

    EtLast.AdoNet.PostgreSql

---
+++ EtLast.AdoNet.PostgreSql/NpgsqlSqlSessionBuilderFluent.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzCode.EtLast;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class NpgsqlSqlSessionBuilderFluent
+{
+    public static ISessionBuilder EnablePostgreSqlClient(this ISessionBuilder session, bool enableLegacyTimestampBehavior)
+    {
+        DbProviderFactories.RegisterFactory("Npgsql", Npgsql.NpgsqlFactory.Instance);
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", enableLegacyTimestampBehavior);
+        return session;
+    }
+}
