f84102ff9e885a46072ea74f1785516a545fe653
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-01-19 07:47:49 +0100

    small fixes

---
+++ EtLast.LocalFiles/Processes/MoveLocalFileJob.cs
@@ -0,0 +1,50 @@
+﻿namespace FizzCode.EtLast;
+
+public sealed class MoveLocalFileJob : AbstractJob
+{
+    [ProcessParameterMustHaveValue]
+    public required string SourceFileName { get; init; }
+
+    [ProcessParameterMustHaveValue]
+    public required string TargetFileName { get; init; }
+
+    public required bool Overwrite { get; init; }
+
+    public override string GetTopic() => SourceFileName;
+
+    protected override void ExecuteImpl(Stopwatch netTimeStopwatch)
+    {
+        try
+        {
+            if (File.Exists(TargetFileName))
+            {
+                if (!Overwrite)
+                    return;
+            }
+
+            File.Move(SourceFileName, TargetFileName, true);
+        }
+        catch (Exception ex)
+        {
+            var exception = new MoveFileException(this, ex);
+            exception.Data["SourceFileName"] = SourceFileName;
+            exception.Data["TargetFileName"] = TargetFileName;
+            exception.Data["Overwrite"] = Overwrite;
+            throw exception;
+        }
+    }
+}
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class MoveLocalFileJobFluent
+{
+    public static IFlow MoveLocalFile(this IFlow builder, string sourceFileName, string targetFileName, bool overwrite)
+    {
+        return builder.ExecuteProcess(() => new MoveLocalFileJob()
+        {
+            SourceFileName = sourceFileName,
+            TargetFileName = targetFileName,
+            Overwrite = overwrite,
+        });
+    }
+}
--- EtLast/Global.cs
+++ EtLast/Global.cs
@@ -11,6 +11,5 @@ global using System.Security;
 global using System.Security.Cryptography;
 global using System.Text;
 global using System.Text.Json;
-global using System.Text.Json.Serialization.Metadata;
 global using System.Threading;
 global using System.Transactions;
