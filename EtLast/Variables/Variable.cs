0524e39d333c09e1077bcb3213f63c955cd7ba91
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-01-19 13:36:31 +0100

    variables 1.0

---
+++ EtLast/Variables/Variable.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzCode.EtLast;
+
+[DebuggerDisplay("{ToString()}")]
+public class Variable<T>(string name, T initialValue = default)
+{
+    public string Name { get; } = name;
+    public T Value { get; set; } = initialValue;
+
+    public override string ToString()
+    {
+        return (Name ?? "var") + "=" + ValueFormatter.Default.Format(Value) + " (" + typeof(T).GetFriendlyTypeName() + ")";
+    }
+}
