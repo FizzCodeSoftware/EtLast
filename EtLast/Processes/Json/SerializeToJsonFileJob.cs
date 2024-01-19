206a081f7509e315c94f67b6027289f9497e1993
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-01-19 10:39:58 +0100

    DeserializeFromJsonFileJob

--- EtLast/Processes/Json/SerializeToJsonFileJob.cs
+++ EtLast/Processes/Json/SerializeToJsonFileJob.cs
@@ -24,6 +24,18 @@ public sealed class SerializeToJsonFileJob<T> : AbstractJob
 
             namedSink.Stream.Write(Encoding.GetBytes(content));
             namedSink.IncreaseRowsWritten();
+
+            namedSink.IoCommand.AffectedDataCount += 1;
+            namedSink.IoCommand.End();
+        }
+        catch (Exception ex)
+        {
+            var exception = new JsonSerializerException(this, "error while serializing into a json file", ex);
+            exception.AddOpsMessage(string.Format(CultureInfo.InvariantCulture, "error while serializing into a json file: {0}", namedSink.Name));
+            exception.Data["StreamName"] = namedSink.Name;
+
+            namedSink.IoCommand.Failed(exception);
+            throw exception;
         }
         finally
         {
@@ -31,5 +43,14 @@ public sealed class SerializeToJsonFileJob<T> : AbstractJob
             namedSink.Stream.Close();
             namedSink.Stream.Dispose();
         }
+    }
+}
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class SerializeToJsonFileJobFluent
+{
+    public static IFlow SerializeToJsonFile<T>(this IFlow builder, Func<SerializeToJsonFileJob<T>> processCreator)
+    {
+        return builder.ExecuteProcess(processCreator);
     }
 }
