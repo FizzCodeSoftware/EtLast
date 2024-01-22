67403c17c72aa9cd500db1659d0b8ac6e6dd4f7b
Zsolt Tóth <zsolt.toth@fizzcode.hu>
2024-01-22 11:14:15 +0100

    Added package references to Db2 project (FizzCode.LightWeight.AdoNet.DbConnection, Roslynator.Formatting.Analyzers) Fixed Db2 package id and extension method name

--- EtLast.AdoNet.Db2/Db2SessionBuilderFluent.cs
+++ EtLast.AdoNet.Db2/Db2SessionBuilderFluent.cs
@@ -3,7 +3,7 @@
 [EditorBrowsable(EditorBrowsableState.Never)]
 public static class Db2SessionBuilderFluent
 {
-    public static ISessionBuilder EnableMicrosoftSqlClient(this ISessionBuilder session)
+    public static ISessionBuilder EnableDb2SqlClient(this ISessionBuilder session)
     {
         DbProviderFactories.RegisterFactory("IBM.Data.Db2", DB2Factory.Instance);
         return session;
