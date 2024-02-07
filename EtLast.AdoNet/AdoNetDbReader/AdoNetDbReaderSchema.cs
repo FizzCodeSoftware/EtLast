93dec355f02529654d1a8d085dda63fcbef3f215
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-02-07 16:31:11 +0100

    fix AdoNetDbReaderColumnSchema

--- EtLast.AdoNet/AdoNetDbReader/AdoNetDbReaderSchema.cs
+++ EtLast.AdoNet/AdoNetDbReader/AdoNetDbReaderSchema.cs
@@ -1,24 +1,42 @@
-﻿namespace FizzCode.EtLast;
+﻿using System.Text.Json.Serialization;
+
+namespace FizzCode.EtLast;
 
 public class AdoNetDbReaderColumnSchema
 {
-    [System.Text.Json.Serialization.JsonIgnore]
+    [JsonIgnore]
     public Type ClrType { get; init; }
 
     public string ClrTypeName { get; init; }
     public string DataTypeName { get; init; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? AllowNull { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public short? Precision { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public short? Scale { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? Size { get; init; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsUnique { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsKey { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsIdentity { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsAutoIncrement { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? IsRowVersion { get; init; }
 
-    [System.Text.Json.Serialization.JsonIgnore]
+    [JsonIgnore]
     public Dictionary<string, string> AllProperties { get; init; }
 }
