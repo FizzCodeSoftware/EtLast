75104505ab41b843505f5ca80da30d1a5704b431
Lengyel János Zoltán <lengyel.janos@gmail.com>
2024-08-30 14:37:32 +0200

    further compress ColumnDataTypeInfo

--- EtLast/ColumnDataTypeInfo.cs
+++ EtLast/ColumnDataTypeInfo.cs
@@ -7,10 +7,10 @@ public class ColumnDataTypeInfo
     public string Name { get; init; }
 
     [JsonIgnore]
-    public Type ClrType { get; init; }
+    public Type ExactClrType { get; init; }
 
-    public string ClrTypeName { get; init; }
-    public string DataTypeName { get; init; }
+    public string ClrType { get; init; }
+    public string DataType { get; init; }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public bool? AllowNull { get; init; }
@@ -21,29 +21,26 @@ public class ColumnDataTypeInfo
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public short? Scale { get; init; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public int? Size { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool IsUnique { get; init; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? IsUnique { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool IsKey { get; init; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? IsKey { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool IsIdentity { get; init; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? IsIdentity { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool IsAutoIncrement { get; init; }
 
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? IsAutoIncrement { get; init; }
-
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public bool? IsRowVersion { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public bool IsRowVersion { get; init; }
 
     [JsonIgnore]
     public Dictionary<string, string> AllProperties { get; init; }
 
     public override string ToString()
     {
-        return Name + ", " + ClrTypeName + ", " + DataTypeName + (Precision != null && Scale != null ? " (" + Precision.Value.ToString(CultureInfo.InvariantCulture) + ", " + Scale.Value.ToString(CultureInfo.InvariantCulture) + ")" : "") + (Size != null ? " [" + Size.Value.ToString(CultureInfo.InvariantCulture) + "]" : "");
+        return Name + ", " + ClrType + ", " + DataType + (Precision != null && Scale != null ? " (" + Precision.Value.ToString(CultureInfo.InvariantCulture) + ", " + Scale.Value.ToString(CultureInfo.InvariantCulture) + ")" : "");
     }
 }
